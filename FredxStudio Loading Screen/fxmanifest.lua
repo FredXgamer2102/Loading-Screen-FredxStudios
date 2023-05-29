8454c17a375740959cfaf67ae2564dbd9d8cfe74
Fredx <108567139+FredXgamer2102@users.noreply.github.com>
2023-05-29 13:07:47 +0100

    Add files via upload

---
+++ FredxStudio Loading Screen/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "FredX#5223"
+description "Loading Screen By FredxStudio"
+version "1.0"
+
+files {
+    'index.html',
+    'audio/*.mp3',
+    'css/styles.css',
+    'video/*',
+    'img/*',
+    'img/*/*',
+    'js/main.js',
+    'video/video.mp4',
+}
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
